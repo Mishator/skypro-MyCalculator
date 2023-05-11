5133ffbd11c03ed80e1711cb4535b185d1493057
Mishator <voyteh89@mail.ru>
2023-05-11 20:48:35 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyproMyCalculator/SkyproMyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproMyCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproMyCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproMyCalculatorApplication.class, args);
+	}
+
+}
