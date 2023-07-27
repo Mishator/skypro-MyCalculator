77f4e788eb1e0460d7d8196bed9721b59a036996
Mishator <voyteh89@mail.ru>
2023-07-27 12:32:49 +0300

    Добавили исключение IllegalStateException

--- src/main/java/pro/sky/skyproMyCalculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/skyproMyCalculator/controller/CalculatorController.java
@@ -37,7 +37,7 @@ public class CalculatorController {
         @GetMapping("/divide")
         public String divide (@RequestParam int num1,@RequestParam int num2){
         if (num2==0) {
-            return "Второй аргумепнт 0, на 0 делить нельзя.";
+            throw new IllegalStateException("На ноль делить нельзя");
         }
             int result = calculatorService.divide(num1, num2);
             return buildResponse(num1, num2, result, '/');
