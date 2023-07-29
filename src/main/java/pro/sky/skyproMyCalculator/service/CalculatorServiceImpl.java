59fe9f9d80d5076ea2e708ece9c6cd0e584df13c
Mishator <voyteh89@mail.ru>
2023-07-29 18:50:53 +0300

    Написаны юнит тесты.

--- src/main/java/pro/sky/skyproMyCalculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/skyproMyCalculator/service/CalculatorServiceImpl.java
@@ -23,10 +23,10 @@ public class CalculatorServiceImpl implements CalculatorService {
     @Override
     public String divide(int num1, int num2) {
         if (num2 == 0) {
-            throw new IllegalStateException("На ноль делить нельзя");
+            throw new IllegalStateException("На ноль делить нельзя!");
         } else {
-            double result = (double) num1 / num2;
-            return stringResult(num1, num2, String.valueOf(num1 / num2), "/");
+           double result = (double) num1 / num2;
+            return stringResult(num1, num2, String.valueOf(result), "/");
         }
     }
 
--- src/test/java/pro/sky/skyproMyCalculator/TestUtils.java
+++ src/test/java/pro/sky/skyproMyCalculator/TestUtils.java
@@ -1,9 +1,7 @@
 package pro.sky.skyproMyCalculator;
 
 public class TestUtils {
-    public static String buildResponse(int num1, int num2, int result, char action) {
+    public static String stringResult(int num1, int num2, String result, String action) {
         return String.format("%s %s %s = %s", num1, action, num2, result);
     }
-
-
 }
